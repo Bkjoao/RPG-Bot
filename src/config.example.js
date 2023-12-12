d9bf91c7084fdb8412b5e4f233d411b7c606e6ba
Bkjoao <131044832+Bkjoao@users.noreply.github.com>
2023-12-12 14:48:20 -0300

    config.js

--- src/config.example.js
+++ src/config.example.js
@@ -2,3 +2,4 @@ const prefix = ['!']; // You can use multiple prefix eg. !, ., -, ?
 const owner_id = ['557798132703']; // Put your number with code country eg. 81xxxxxxxx
 
 export { prefix, owner_id };
+ 
